5edd7b103b7442da3e8454f50f3c3ab6f5a810f7
kneepdee <gabriel.kuslap@gmail.com>
2019-02-26 18:59:16 +0200

    Finish 6.3

--- week-6/6.3a Rekursiivne kuup.py
+++ week-6/6.3a Rekursiivne kuup.py
@@ -5,10 +5,16 @@
 #     return kuup_jarj
 
 
-def kuup(järjend, tulemus=[]):
-    if len(järjend) > 0:
-        return kuup(järjend[1:], [järjend[0] ** 3] + tulemus)[::-1]
-    return tulemus
+# def summa_sabarekursioon_lõige(järjend, tulemus=0):
+#     if len(järjend) > 0:
+#         return summa_sabarekursioon_lõige(järjend[1:], järjend[0] + tulemus)
+#     return tulemus
+
+def kuup(jarj):
+    if len(jarj) == 0:
+        return jarj
+    return [jarj[0] ** 3] + kuup(jarj[1:])
 
 
-print(kuup([1, 2, 3, 4, 5]))
+print(kuup([1, 2, 3]))
+# print(summa_sabarekursioon_lõige([1, 2, 3]))
