08efb51ec345abdd5e64738707e9ce540ba8ca97
kneepdee <gabriel.kuslap@gmail.com>
2019-02-26 11:39:35 +0200

    finish 6.2

--- week-6/6.2 Keerulised suhted.py
+++ week-6/6.2 Keerulised suhted.py
@@ -1,6 +1,32 @@
 def seosta_lapsed_ja_vanemad(laste_faili_nimi, nimede_faili_nimi):
+    lapsed_fail = open(laste_faili_nimi)
+    nimed_fail = open(nimede_faili_nimi)
+    suhted = {}
+    laste_nimed = set()
+
+    nimed = {}
+
+    for rida in nimed_fail:
+        isikukood = rida.split()[0]
+        nimi_listina = rida.split()[1:]
+        nimi = ' '.join(nimi_listina)
+        nimed[isikukood] = nimi
+
+    for rida in lapsed_fail:
+        vanem = nimed[rida.split()[0]]
+        laps = nimed[rida.split()[1]]
+        if vanem in suhted:
+            suhted[vanem].add(laps)
+        else:
+            suhted[vanem] = set()
+            suhted[vanem].add(laps)
 
     return suhted
 
 
-seosta_lapsed_ja_vanemad('lapsed.txt', 'nimed.txt')
+# print(seosta_lapsed_ja_vanemad('./week-6/lapsed.txt', './week-6/nimed.txt'))
+
+# {'Peeter Peedumets': {'Maria Peedumets', 'Robert Peedumets'},
+# 'Madli Peedumets': {'Maria Peedumets', 'Robert Peedumets'},
+# 'Karl Peedumets': {'Peeter Peedumets'},
+# 'Kadri Kalkun': {'Liisa Maria Jaaniste'}}
