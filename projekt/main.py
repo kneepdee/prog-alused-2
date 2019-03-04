e28db4a7a5acc1eb1ba27090956baa5754ddd3f8
kneepdee <gabriel.kuslap@gmail.com>
2019-03-04 22:21:26 +0200

    All df-s start from 2004

--- projekt/main.py
+++ projekt/main.py
@@ -7,14 +7,17 @@ def loo_dataframe(failinimi, näitaja):
     andmed = pd.read_csv('./data/' + failinimi,
                          delimiter=',', index_col='Vaatlusperiood')
 
+    # jätame df-i alles ainult meid huvitavale näitajale vastavad andmed
     näitajapõhised_andmed = andmed.ix[andmed['Näitaja'] ==
                                       näitaja]
 
+    # vabaneme nendest tulpadest, mida vaja ei ole
     näitajapõhised_andmed = näitajapõhised_andmed.drop(
         ['DIM1', 'Flag Codes', 'Flags', 'Näitaja'], axis=1)
 
-    if näitajapõhised_andmed['TIME'].iloc[0] < 2004:
-        näitajapõhised_andmed = näitajapõhised_andmed[4:]
+    # alustame df-i aastast 2004, et plotid ilusad välja näeksid
+    näitajapõhised_andmed = näitajapõhised_andmed[pd.Index(
+        list(näitajapõhised_andmed['TIME'])).get_loc(2004):]
 
     return näitajapõhised_andmed
 
@@ -42,13 +45,24 @@ sisevete_kalapüügi_andmed = loo_dataframe(
 sisevete_kalapüügi_andmed['Value'] = korruta_tulba_väärtused(
     sisevete_kalapüügi_andmed['Value'], 6)
 
-plot.plot(skp_andmed['Value'],
-          label='Sisemajanduse koguprodukt elaniku kohta, eurot')
-plot.plot(jäätmete_tekke_andmed['Value'], label='Jäätmete teke, tuhat tonni')
-plot.plot(sisevete_kalapüügi_andmed['Value'],
-          label='Sisevete kalapüük, 1/6 tonni')
-plot.plot(põllumajandusmaa_andmed['Value'],
-          label='Intensiivpõllumajanduse kasutuses olev maa, 1/2500% territooriumist')
+f_gaaside_heitekogus_andmed = loo_dataframe(
+    'kliimamuutus.csv', 'F-gaaside heitkogus, CO2 ekvivalenttonni')
+
+f_gaaside_heitekogus_andmed['Value'] = korruta_tulba_väärtused(
+    f_gaaside_heitekogus_andmed['Value'], 105)
+
+# plot.plot(skp_andmed['Value'],
+#           label='Sisemajanduse koguprodukt elaniku kohta, eurot')
+# plot.plot(jäätmete_tekke_andmed['Value'], label='Jäätmete teke, tuhat tonni')
+# plot.plot(sisevete_kalapüügi_andmed['Value'],
+#           label='Sisevete kalapüük, 1/6 tonni')
+# plot.plot(põllumajandusmaa_andmed['Value'],
+#           label='Intensiivpõllumajanduse kasutuses olev maa, 1/2500% territooriumist')
+# plot.plot(f_gaaside_heitekogus_andmed['Value'],
+#           label='F-gaaside heitkogus, CO2 ekvivalenttonni')
 
 # plot.legend()
-plot.show()
+# plot.show()
+
+# todo: funktsioon, mis automaatselt scalib väärtused skt kõveraga sobivale skaalale
+# todo: kliimamuutusest f-gaaside heitekoguse andmed
